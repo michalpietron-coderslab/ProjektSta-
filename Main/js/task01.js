b2680bee11c389248a71ac30782068d4d4ad4545
Łukasz Kowalik <lukaszkowalik25@gmail.com>
2020-03-16 21:37:39 +0100

    Pierwszy fetch działa

--- Main/js/task01.js
+++ Main/js/task01.js
@@ -4,45 +4,112 @@ import ReactDOM from "react-dom";
 //import "style.scss";
 const api = "https://recruitment.hal.skygate.io/companies";
 console.log(api);
+const api2 = "https://recruitment.hal.skygate.io/incomes/:id";
+console.log(api2);
 
 class Companies extends Component {
-  state = {
-    data: false,
-    id: false
-  };
+  constructor(props) {
+    super(props);
+  }
+  state = { isLoading: false, error: null, data: [] };
 
   componentDidMount() {
-    const zzz = fetch(api)
-      .then((r) => r.text())
-      .then((ip) => {
-        this.setState({
-          data: ip,
-          id: ip
-        });
-        method: "GET";
+    this.setState({ isLoading: true });
+
+    fetch(api)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+          console.log(response.json());
+        } else {
+          throw Error("Błąd!");
+        }
       })
-      .catch((err) => {
-        console.log("Błąd!", err);
-      });
-    console.log(zzz);
+      .then((data) => this.setState({ data, isLoading: false }))
+      .catch((error) => this.setState({ error }));
   }
 
   render() {
-    if (!this.state.data) return <h1>Ustalam adres IP...</h1>;
+    const { error, isLoading, data } = this.state;
+
+    if (error) {
+      return <h1>{this.state.error.mesage}</h1>;
+    }
+    if (isLoading) return <h1>Ustalam adres IP...</h1>;
     return (
-      <>
-        <h1>Twoje IP: {this.state.data}</h1>
-        <ul>
-          {
-            <li>
-              hggjfggj
-              {this.state.id}
-            </li>
-          }
-        </ul>
-      </>
+      <div>
+        {data.map((dates) => (
+          <div key={dates.id}>
+            <>
+              <tbody>
+                {/* <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>CIty</th>
+                <th>Total Income</th>
+              </tr> */}
+                <tr>
+                  <th>{dates.id}</th>
+                  <th>{dates.name}</th>
+                  <th>{dates.city}</th>
+                  <th>{dates.id}</th>
+                </tr>
+              </tbody>
+            </>
+          </div>
+        ))}
+      </div>
     );
   }
 }
 
 ReactDOM.render(<Companies />, document.getElementById("app"));
+
+// import React, { Component } from "react";
+// import ReactDOM from "react-dom";
+
+// //import "style.scss";
+// const api = "https://recruitment.hal.skygate.io/companies";
+// console.log(api);
+// const api2 = "https://recruitment.hal.skygate.io/incomes/:id";
+// console.log(api2);
+
+// class Companies extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   state = { isLoading: false, error: null, data: [] };
+
+//   componentDidMount() {
+//     this.setState({ isLoading: true });
+
+//     fetch(api)
+//       .then((response) => {
+//         if (response.ok) {
+//           return response.json();
+//         } else {
+//           throw Error("Błąd!");
+//         }
+//       })
+//       .then((data) => this.setState({ data, isLoading: false }))
+//       .catch((error) => this.setState({ error }));
+//   }
+
+//   render() {
+//     const { error, isLoading, data } = this.state;
+
+//     if (error) {
+//       return <h1>{this.state.error.mesage}</h1>;
+//     }
+//     if (isLoading) return <h1>Ustalam adres IP...</h1>;
+//     return (
+//       <div>
+//         {data.map((dates) => (
+//           <p>dates.name</p>
+//         ))}
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<Companies />, document.getElementById("app"));
