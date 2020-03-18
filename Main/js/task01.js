37648d35a8899f98f00adf3f0cd99ed83fd2cf4e
Łukasz Kowalik <lukaszkowalik25@gmail.com>
2020-03-18 17:16:15 +0100

    Dodano drugie Api

--- Main/js/task01.js
+++ Main/js/task01.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-//import "style.scss";
+import "../style.scss";
+
 const api = "https://recruitment.hal.skygate.io/companies";
-console.log(api);
-const api2 = "https://recruitment.hal.skygate.io/incomes/:id";
-console.log(api2);
+// console.log(api);
+const api2 = (id) => `https://recruitment.hal.skygate.io/incomes/${id}`;
+// console.log(api2);
 
 class Companies extends Component {
   constructor(props) {
     super(props);
   }
-  state = { isLoading: false, error: null, data: [] };
+  state = { isLoading: false, error: null, data: [], incomes: [] };
 
   componentDidMount() {
     this.setState({ isLoading: true });
@@ -25,91 +26,56 @@ class Companies extends Component {
           throw Error("Błąd!");
         }
       })
-      .then((data) => this.setState({ data, isLoading: false }))
+      .then((data) =>
+        this.setState({ data, isLoading: false }, () => {
+          const urls = this.state.data.map((item) => api2(item.id));
+          this.getAllUrls(urls);
+        })
+      )
       .catch((error) => this.setState({ error }));
   }
 
+  getAllUrls = (urls) => {
+    Promise.all(
+      urls.map((url) => fetch(url).then((response) => response.json()))
+    ).then((incomes) => this.setState({ incomes }));
+  };
+
   render() {
-    const { error, isLoading, data } = this.state;
+    const { error, isLoading, data, incomes } = this.state;
+
+    const temData = [];
+    for (let i = 0; i < data.length; i++) {
+      temData.push({
+        ...data[i],
+        ...incomes[i]
+      });
+    }
+    console.log(temData);
 
     if (error) {
       return <h1>{this.state.error.mesage}</h1>;
     }
     if (isLoading) return <h1>Ustalam adres IP...</h1>;
     return (
-      <div>
-        {data.map((dates) => (
-          <div key={dates.id}>
-            <>
-              <tbody>
-                {/* <tr>
-                <th>ID</th>
-                <th>NAME</th>
-                <th>CIty</th>
-                <th>Total Income</th>
-              </tr> */}
-                <tr>
-                  <th>{dates.id}</th>
-                  <th>{dates.name}</th>
-                  <th>{dates.city}</th>
-                  <th>{dates.id}</th>
-                </tr>
-              </tbody>
-            </>
-          </div>
+      <table className="mainTable">
+        <tr>
+          <td>ID</td>
+          <td>NAME</td>
+          <td>CITY</td>
+          <td>TOTAL INCOME</td>
+        </tr>
+        {temData.map((dates) => (
+          <tr key={dates.id}>
+            <th>{dates.id}</th>
+            <th>{dates.name}</th>
+            <th>{dates.city}</th>
+            <th></th>
+          </tr>
         ))}
-      </div>
+      </table>
     );
   }
 }
 
 ReactDOM.render(<Companies />, document.getElementById("app"));
-
-// import React, { Component } from "react";
-// import ReactDOM from "react-dom";
-
-// //import "style.scss";
-// const api = "https://recruitment.hal.skygate.io/companies";
-// console.log(api);
-// const api2 = "https://recruitment.hal.skygate.io/incomes/:id";
-// console.log(api2);
-
-// class Companies extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-//   state = { isLoading: false, error: null, data: [] };
-
-//   componentDidMount() {
-//     this.setState({ isLoading: true });
-
-//     fetch(api)
-//       .then((response) => {
-//         if (response.ok) {
-//           return response.json();
-//         } else {
-//           throw Error("Błąd!");
-//         }
-//       })
-//       .then((data) => this.setState({ data, isLoading: false }))
-//       .catch((error) => this.setState({ error }));
-//   }
-
-//   render() {
-//     const { error, isLoading, data } = this.state;
-
-//     if (error) {
-//       return <h1>{this.state.error.mesage}</h1>;
-//     }
-//     if (isLoading) return <h1>Ustalam adres IP...</h1>;
-//     return (
-//       <div>
-//         {data.map((dates) => (
-//           <p>dates.name</p>
-//         ))}
-//       </div>
-//     );
-//   }
-// }
-
-// ReactDOM.render(<Companies />, document.getElementById("app"));
