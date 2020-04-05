5d845ab37c0c631eccbe5a9e7b38eaaf1ad3883e
szkolenia <clear>
2020-04-05 14:17:01 +0200

    React-bootstrap-table

--- Main/js/Components/Table.js
+++ Main/js/Components/Table.js
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+
+
+import '../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+
 
 import CompanyItem from './CompanyItem';
+
+var products = [{
+  id: 1,
+  name: "Product1",
+  price: 120
+}, {
+  id: 2,
+  name: "Product2",
+  price: 80
+}];
 
 
 export default class Table extends Component {
@@ -11,26 +26,45 @@ export default class Table extends Component {
   }
 
 
+  handleBtnClick = () => {
+    if (order === 'desc') {
+      this.refs.table.handleSort('asc', 'name');
+      order = 'asc';
+    } else {
+      this.refs.table.handleSort('desc', 'name');
+      order = 'desc';
+    }
+  }
+
+
   render() {
     console.log('Render TableComponent');
     return (
-      <div>
-        <table className="mainTable">
-          <thead>
-            <tr>
-              <td>ID</td>
-              <td>NAME</td>
-              <td>CITY</td>
-              <td>TOTAL INCOME</td>
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.data.map((item, index) => (
-              <CompanyItem key={item.id} data={item} />
-            ))}
-          </tbody>
-        </table>
-      </div>
+
+      <BootstrapTable ref='table' data={this.props.data}>
+        <TableHeaderColumn dataField='id' isKey={true} dataSort={true}>Company ID</TableHeaderColumn>
+        <TableHeaderColumn dataField='name' dataSort={true}>Company Name</TableHeaderColumn>
+        <TableHeaderColumn dataField='city' dataSort={true}>Company City</TableHeaderColumn>
+        <TableHeaderColumn dataField='income' dataSort={true}>Total Income</TableHeaderColumn>
+      </BootstrapTable>
+
+      // <div>
+      //   <table className="mainTable">
+      //     <thead>
+      //       <tr>
+      //         <td>ID</td>
+      //         <td>NAME</td>
+      //         <td>CITY</td>
+      //         <td>TOTAL INCOME</td>
+      //       </tr>
+      //     </thead>
+      //     <tbody>
+      //       {this.props.data.map((item, index) => (
+      //         <CompanyItem key={item.id} data={item} />
+      //       ))}
+      //     </tbody>
+      //   </table>
+      // </div>
     );
   }
 }
